6f1d9a55403582ef2fa907ab53bd68a6a0d8071c
CoffeeGopher <coffeegopher1@gmail.com>
2022-09-03 12:33:17 -0700

    Implemented getPiFromSideCount method and printed the results from 1 to 1000

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class ArchimedesPiMethod {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println("Number of sides: " + i + ", Pi approx.: " + getPiFromSideCount(i));
+
+        }
+
+        int numberOfSidesForAnswer = 8;
+        System.out.println("\nFINAL ANSWER:\nNumber of sides: " + numberOfSidesForAnswer + ", Pi approx.: "+ getPiFromSideCount(numberOfSidesForAnswer));
+    }
+
+    static double getPiFromSideCount(int sideCount) {
+        double segmentAngle = 360.0 / sideCount;
+        double triangleBase = 2 * sin(toRadians(segmentAngle / 2));
+        double polygonPerimeter = triangleBase * sideCount;
+        return polygonPerimeter / 2;
     }
 }
